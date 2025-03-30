fc52009d86b034dd0f6cd3a7338885c24c2ee7f9
Raghav Atreya <atreyaraghav@gmail.com>
2025-03-30 19:27:44 +0530

    path and url for register_user view added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from accounts.views import forgot_password
+from accounts.views import forgot_password, register_user
 
 urlpatterns = [
     path("forgot-password/", forgot_password, name="forgot_password"),
+    path("register/", register_user, name="register_user"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from accounts.utils import password_strength, verify_email
+from django.http import HttpRequest
 
 
 def forgot_password(request):
@@ -10,3 +11,10 @@ def forgot_password(request):
 
         if not is_valid_email[0]:
             return render(request, "error.html", context={"msg": is_valid_email[1]})
+
+
+def register_user(request: HttpRequest):
+    """"""
+    if request.method == "POST":
+        pass
+    return render(request, "accounts/register.html", context={"msg": msg})
