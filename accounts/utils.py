35ff267c302bbb2e6de11869e6fe988001557018
Raghav Atreya <atreyaraghav@gmail.com>
2025-03-30 10:13:49 +0530

    send email functionality added

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,6 +1,7 @@
 import string
 from typing import Tuple
 from accounts.models import BlackListEmail
+from django.core.mail import send_mail
 
 
 def verify_email(email_str: str) -> Tuple[bool, str]:
@@ -12,12 +13,15 @@ def verify_email(email_str: str) -> Tuple[bool, str]:
 
     email_object = BlackListEmail.objects.filter(email=email_str).first()
 
-    if email_object is None:
-        return False, "Email is in Black List"
+    if email_object is not None:
+        return False, "Email is in Black List, Please contact Administrator"
+
     accepted_domains = ["@sarvika.com"]
 
     for domain in accepted_domains:
-        if email_str.endswith(domain):
+        # email is in accepted domain list and it length should be greater than domains length
+        # malicious user may submit only the domain name
+        if email_str.endswith(domain) and len(email_str) > len(domain):
             return True, "Accepted"
 
     return False, "Email address is not acceptable"
@@ -57,3 +61,55 @@ def password_strength(password_str: str) -> Tuple[bool, str]:
         return True, "Password is not strong"
 
     return True, "Password is Strong"
+
+
+def send_email(
+    subject: str,
+    message: str,
+    email_from: str,
+    email_to: str,
+) -> Tuple[bool, str]:
+    """ """
+    verify_email_sender = verify_email(email_from)
+    if not verify_email_sender[0]:
+        return verify_email_sender
+
+    verify_email_receiver = verify_email(email_to)
+    if not verify_email_sender[0]:
+        return verify_email_receiver
+
+    if message == "" or message is None:
+        return False, "Message cannot be null or blank"
+
+    if subject == "" or subject is None:
+        return False, "Subject cannot be null or blank"
+
+    email_send_flag = False
+    email_send_msg = ""
+    try:
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=email_from,
+            recipient_list=[email_to],
+            fail_silently=False,
+        )
+        email_send_flag = True
+        email_send_msg = "Email send"
+    except Exception as e:
+        print(str(e))
+        email_send_flag = False
+        email_send_msg = str(e)
+
+    return email_send_flag, email_send_msg
+
+
+if __name__ == "__main__":
+    print(
+        send_email(
+            subject="first email",
+            message="first message",
+            email_from="raghav.atreya@sarvika.com",
+            email_to="pawan.nagarwal@sarvika.com",
+        )
+    )
